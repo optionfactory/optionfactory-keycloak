0e0c673513f69563bda389cdfa6c558894dd715d
Roberto Ferranti <roberto.ferranti@gmail.com>
2023-05-04 11:27:52 +0200

    :ref: simplified ResourceAuthenticator

--- optionfactory-keycloak-resources-auth/src/main/java/net/optionfactory/keycloak/resources/auth/ResourceAuthenticator.java
+++ optionfactory-keycloak-resources-auth/src/main/java/net/optionfactory/keycloak/resources/auth/ResourceAuthenticator.java
@@ -1,14 +1,22 @@
 package net.optionfactory.keycloak.resources.auth;
 
-import org.keycloak.provider.Provider;
+import java.util.stream.Stream;
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.NotAuthorizedException;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.services.managers.AppAuthManager.BearerTokenAuthenticator;
+import org.keycloak.services.managers.AuthenticationManager.AuthResult;
 
-public interface ResourceAuthenticator extends Provider {
+public interface ResourceAuthenticator {
 
-    void enforceScope(String scope);
-
-    @Override
-    default void close() {
-
+    public static void enforceScope(KeycloakSession session, String scope) {
+        final AuthResult auth = new BearerTokenAuthenticator(session).authenticate();
+        if (auth == null) {
+            throw new NotAuthorizedException("Bearer");
+        }
+        if (Stream.of(auth.getToken().getScope().split(" ")).noneMatch(scope::equals)) {
+            throw new ForbiddenException(String.format("Client does not have required scope '%s'", scope));
+        }
     }
 
 }
--- optionfactory-keycloak-resources-auth/src/main/java/net/optionfactory/keycloak/resources/auth/ResourceAuthenticatorFactory.java
+++
@@ -1,7 +0,0 @@
-package net.optionfactory.keycloak.resources.auth;
-
-import org.keycloak.provider.ProviderFactory;
-
-public interface ResourceAuthenticatorFactory extends ProviderFactory<ResourceAuthenticator> {
-
-}
